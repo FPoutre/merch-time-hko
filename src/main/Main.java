e02704582030f028047bf57e452057973e205a80
Ossama <ossama1998@windowslive.com>
2020-11-16 20:15:13 +0100

    Parse des entrées

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("hloo");
+        final Scanner scanner = new Scanner(System.in) ;
+
+        int amountToSpend = scanner.nextInt();
+        scanner.nextLine();
+        int numberOfItems = scanner.nextInt();
+        scanner.nextLine();
+
+        ArrayList<Integer> items = new ArrayList<>() ;
+        for (int i = 0; i < numberOfItems; i++) {
+            items.add(scanner.nextInt());
+        }
+
+        System.out.println(amountToSpend);
+        System.out.println(numberOfItems);
+        System.out.println(items);
     }
 
 }
