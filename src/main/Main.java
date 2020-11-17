1e7b64c4929cbff19a69ce806c4fcbd3b7058881
fpoutre <kevin.levy@etu.univ-cotedazur.fr>
2020-11-17 19:54:36 +0100

    Added a function to tell if there's a solution for merch-time or not, and fixed a few bugs

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,28 +17,53 @@ public class Main {
             items.add(scanner.nextInt());
         }
 
-        System.out.println(greedy(items , amountToSpend));
+        //System.out.println(greedy(items , amountToSpend));
+        if (solver(items, amountToSpend)) {
+            System.out.println("OUI");
+            System.out.println(greedy(items, amountToSpend));
+        } else {
+            System.out.println("NON");
+        }
+    }
 
+    public static boolean solver(ArrayList<Integer> items, int amountToSpend) {
+        ArrayList<Integer> our_items = greedy(new ArrayList<>(items), amountToSpend);
+        ArrayList<Integer> bobs_items = new ArrayList<>(items);
+        for (int n : our_items) {
+            bobs_items.remove((Object) n);
+        }
+        bobs_items = greedy(bobs_items, amountToSpend);
+
+        int our_amount = 0;
+        int bobs_amount = 0;
+        for (int n : our_items) our_amount += n;
+        for (int n : bobs_items) bobs_amount += n;
+
+        return our_amount > bobs_amount;
     }
 
     public static ArrayList<Integer> greedy(ArrayList<Integer> items, int amountToSpend) {
         try {
-            int chosen_item = Collections.max(items); // We choose the heaviest item
+            ArrayList<Integer> items_copy = new ArrayList<>(items);
+            int chosen_item = Collections.max(items_copy); // We choose the heaviest item
 
-            if (chosen_item <= amountToSpend) { // If the item fits the backpack
+            if (amountToSpend == 0) { // If there's no space left in the backpack
+                // Then return nothing
+                return new ArrayList<>();
+            } else if (chosen_item <= amountToSpend) { // If the item fits the backpack
                 // Put it in the backpack
-                items.remove((Object) chosen_item);
-                ArrayList<Integer> res = greedy(items, amountToSpend-chosen_item);
+                items_copy.remove((Object) chosen_item);
+                ArrayList<Integer> res = greedy(items_copy, amountToSpend-chosen_item);
                 res.add(chosen_item);
                 return res;
             } else {
                 // Else, don't use it
-                items.remove((Object) chosen_item);
-                return greedy(items, amountToSpend);
+                items_copy.remove((Object) chosen_item);
+                return greedy(items_copy, amountToSpend);
             }
         } catch (NoSuchElementException e) { // If there are no more items to add
             // Then return nothing
-            return new ArrayList<Integer>();
+            return new ArrayList<>();
         }
 
     }
